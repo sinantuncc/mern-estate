7849491b0b19c49e8dc7a359dba435122f8c453c
sinantuncc <sinantunc878@gmail.com>
2023-11-21 02:11:28 -0800

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SingUp from './pages/SingUp';
+import SingIn from './pages/SingIn';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
 export default function App() {
-  return <div className='text-red-600'>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/sign-up' element={<SingUp />} />
+        <Route path='/sign-in' element={<SingIn />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

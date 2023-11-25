802b8e8327636f4ddc65cdb8bc50bca1a4d747f3
sinantuncc <sinantunc878@gmail.com>
2023-11-25 22:54:54 +0300

    create user update api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import authRouter from './routes/auth.route.js';
+import userRouter from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -15,8 +17,10 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
